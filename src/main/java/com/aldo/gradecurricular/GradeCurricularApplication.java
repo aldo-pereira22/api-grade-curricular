f7dd527eb5b24e2b3e231536e5059698bbbd608e
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2022-12-19 20:34:20 -0300

    Git ignore

---
+++ src/main/java/com/aldo/gradecurricular/GradeCurricularApplication.java
@@ -0,0 +1,13 @@
+package com.aldo.gradecurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldo/gradecurricular/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldo.gradecurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
