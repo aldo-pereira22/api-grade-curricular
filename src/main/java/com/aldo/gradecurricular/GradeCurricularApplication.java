aaead71a76d4762d86d3119281320d2c29c90f09
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-01-30 21:30:59 -0300

    Criando pacote de controller

--- src/main/java/com/aldo/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/aldo/gradecurricular/GradeCurricularApplication.java
@@ -2,12 +2,22 @@ package com.aldo.gradecurricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class GradeCurricularApplication {
-
-	public static void main(String[] args) {
+public class GradeCurricularApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GradeCurricularApplication.class);
+	}
+	
+	public static void main(String[] args) {	
 		SpringApplication.run(GradeCurricularApplication.class, args);
 	}
+	
+	
 
 }
