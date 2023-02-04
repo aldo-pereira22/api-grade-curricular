8781e4c415fedec397e1749a59b32ef6958b82e4
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-02-04 15:24:50 -0300

    Ajustando o parametro minimo de frequencia

--- src/main/java/com/aldo/gradecurricular/dto/MateriaDto.java
+++ src/main/java/com/aldo/gradecurricular/dto/MateriaDto.java
@@ -15,14 +15,14 @@ public class MateriaDto {
 	@NotBlank(message = "Informe o Nome da matéria")
 	private String nome;
 
-	@Min(value = 34, message = "Permitido mínimo de 34 horas por matéria.")
+	@Min(value = 20, message = "Permitido mínimo de 34 horas por matéria.")
 	@Max(value = 120, message = "Permitido máximo de 120 horas por matéria.")
 	private int horas;
 	
 	@NotBlank(message = "Informe o Código da matéria")
 	private String codigo;
 	
-	@Min(value = 34, message = "Permitido mínimo de 1 vez ao ano.")
-	@Max(value = 120, message = "Permitido máximo de 2 vezess por matéria.")
+	@Min(value = 1, message = "Permitido mínimo de 1 vez ao ano.")
+	@Max(value = 2, message = "Permitido máximo de 2 vezess por matéria.")
 	private int frequencia;
 }
